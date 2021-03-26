2cfde63bc8975923ba40eacbde46b9e954ac3ee5
Max <max.white.rabbit@gmail.com>
2021-03-26 18:46:45 +0500

    refac: place get groups logic to separate function

--- extractor.py
+++ extractor.py
@@ -35,63 +35,62 @@ class NumberExtractor(Extractor):
                 return new_text
         else:
             return None
-    
+
+    def _get_groups(self, text):
+        start = 0
+        matches = list(self.parser.findall(text))
+        groups = []
+        group_matches = []
+
+        for i, match in enumerate(matches):
+            if i == 0:
+                start = match.span.start
+            if i == len(matches) - 1:
+                next_match = match
+            else:
+                next_match = matches[i + 1]
+            group_matches.append(match.fact)
+            if text[match.span.stop: next_match.span.start].strip() or next_match == match:
+                groups.append((group_matches, start, match.span.stop))
+                group_matches = []
+                start = next_match.span.start
+        return groups
+
+
     def replace_groups(self, text):
         """
-        Замена сгруппированных составных чисел в тексте
+        Replace groups of numerical by summation
 
-        Аргументы:
-            text: исходный текст
+        Args:
+            text: initial text
 
-        Результат:
-            new_text: текст с замененными числами
+        Result:
+            new_text: text with numbers
         """
-        if text:
-            start = 0
-            matches = list(self.parser.findall(text))
-            groups = []
-            group_matches = []
-
-            for i, match in enumerate(matches):
-                if i == 0:
-                    start = match.span.start
-                if i == len(matches) - 1:
-                    next_match = match
-                else:
-                    next_match = matches[i + 1]
-                group_matches.append(match.fact)
-                if text[match.span.stop: next_match.span.start].strip() or next_match == match:
-                    groups.append((group_matches, start, match.span.stop))
-                    group_matches = []
-                    start = next_match.span.start
-            
-            new_text = ""
-            start = 0
-
-            for group in groups:
-                num = 0
-                nums = []
-                new_text += text[start: group[1]]
-                for match in group[0]:
-                    curr_num = match.int * match.multiplier if match.multiplier else match.int
-                    if match.multiplier:
-                        num = (num + match.int) * match.multiplier
-                        nums.append(num)
-                        num = 0
-                    elif num > curr_num or num == 0:
-                        num += curr_num
-                    else:
-                        nums.append(num)
-                        num = 0
-                if num > 0:
+        
+        groups = self._get_groups(text)
+        
+        new_text = ""
+        start = 0
+        for group in groups:
+            num = 0
+            nums = []
+            new_text += text[start: group[1]]
+            for match in group[0]:
+                curr_num = match.int * match.multiplier if match.multiplier else match.int
+                if match.multiplier:
+                    num = (num + match.int) * match.multiplier
                     nums.append(num)
-                new_text += str(sum(nums))
-                start = group[2]
-            new_text += text[start:]
+                    num = 0
+                elif num > curr_num or num == 0:
+                    num += curr_num
+                else:
+                    nums.append(num)
+                    num = 0
+            if num > 0:
+                nums.append(num)
+            new_text += str(sum(nums))
+            start = group[2]
+        new_text += text[start:]
 
-            if start == 0:
-                return text
-            else:
-                return new_text
-        else:
-            return None
+        return new_text
